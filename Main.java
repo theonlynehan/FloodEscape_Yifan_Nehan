2f5fdea4ecabcf37d95be15a57fac5b72c818391
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2022-12-25 19:22:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
 
 public class Main {
-
+  public static void main(String[] args) {
+    // this is just for testing purposes
+    System.out.println("hi");
+    JFrame bruh = new JFrame();
+    Menu.displayMenu(bruh);
+    bruh.setVisible(true);
+  }
 }
