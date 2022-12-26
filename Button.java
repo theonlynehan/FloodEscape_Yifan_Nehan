2be26bcd50287ff2590d9ea4c16aabd65e28fc2a
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2022-12-25 19:21:34 -0500

    Update Button.java

--- Button.java
+++ Button.java
@@ -2,27 +2,19 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Button extends JButton implements ActionListener {
-  private boolean isStopped; 
+public class Button extends Obj {
+  private static ImageIcon btnNotPressed = new ImageIcon("file2.png"); // will add the images later
+  private static ImageIcon btnPressed = new ImageIcon("file.png");
+  private boolean isStopped;
   private Water water;
-  private Player player; 
+  private Player player;
 
-  public Button(Water water, Player player) {
-    this.water = water;
-    this.player = player;
-    this.isStopped = false;
-    this.setText("Press Me!");
-    this.addActionListener(this);
+  public Button() {
+    super(); // this doesn't work yet
+    this.setIcon(btnNotPressed);
   }
 
-  public void actionPerformed(ActionEvent e) {
-    if (!isStopped && playerWithinBounds()) {
-      this.setText("You Won!");
-      this.isStopped = true;
-      water.stopWater();
-    }
-  }
-
+  // we will change this later so that the player checks for all collisions
   public boolean playerWithinBounds() {
     int playerX = player.getX();
     int playerY = player.getY();
@@ -32,7 +24,8 @@ public class Button extends JButton implements ActionListener {
     int buttonWidth = this.getWidth();
     int buttonHeight = this.getHeight();
 
-    if (playerX >= buttonX && playerX <= buttonX + buttonWidth && playerY      >= buttonY && playerY <= buttonY + buttonHeight) {
+    if (playerX >= buttonX && playerX <= buttonX + buttonWidth && playerY >= buttonY
+        && playerY <= buttonY + buttonHeight) {
       return true;
     } else {
       return false;
@@ -43,7 +36,7 @@ public class Button extends JButton implements ActionListener {
     g.setColor(Color.RED);
     g.fillRect(getX(), getY(), getWidth(), getHeight());
     g.setColor(Color.BLACK);
-    g.drawString(getText(), getX()+getWidth()/2, getY() + getHeight() / 2);
+    g.drawString(getText(), getX() + getWidth() / 2, getY() + getHeight() / 2);
   }
 
 }
