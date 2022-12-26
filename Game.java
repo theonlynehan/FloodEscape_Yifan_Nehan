7a8f3b95ca669c16ca45672ee56b0f58df6fd30d
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2022-12-25 19:22:05 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,36 +1,42 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Game {
-  private Button button;
-  private Map map;
-  private Menu menu;
-  private Player player;
+public class Button extends Obj {
+  private static ImageIcon btnNotPressed = new ImageIcon("file2.png"); // will add the images later
+  private static ImageIcon btnPressed = new ImageIcon("file.png");
+  private boolean isStopped;
   private Water water;
-  
-  public Game(Water water, Player player) {
-	player = new Player();
-	water = new Water();
-    button = new Button(water, player);
-    map = new Map(button);
-    menu = new Menu();
-  }
-  
-  public void runGame() {
-    
-    while (true) {
-      water.rise();
-      player.move();
-      
+  private Player player;
 
-      
-      repaint();
+  public Button() {
+    super(); // this doesn't work yet
+    this.setIcon(btnNotPressed);
+  }
+
+  // we will change this later so that the player checks for all collisions
+  public boolean playerWithinBounds() {
+    int playerX = player.getX();
+    int playerY = player.getY();
+
+    int buttonX = this.getX();
+    int buttonY = this.getY();
+    int buttonWidth = this.getWidth();
+    int buttonHeight = this.getHeight();
+
+    if (playerX >= buttonX && playerX <= buttonX + buttonWidth && playerY >= buttonY
+        && playerY <= buttonY + buttonHeight) {
+      return true;
+    } else {
+      return false;
     }
   }
-  
-  public void repaint(Graphics g) {
-    map.render(g);
-    player.render(g);
-    water.render(g);
+
+  public void render(Graphics g) {
+    g.setColor(Color.RED);
+    g.fillRect(getX(), getY(), getWidth(), getHeight());
+    g.setColor(Color.BLACK);
+    g.drawString(getText(), getX() + getWidth() / 2, getY() + getHeight() / 2);
   }
+
 }
