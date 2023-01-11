47449c8e0fbb2d63ac6e97921afecafc8c58939a
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2023-01-10 22:47:51 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,41 +1,20 @@
-import java.awt.Color;
-import java.util.ArrayList;
-import java.awt.Graphics;
-
-// okay let me send u what i got in discord this is confusing
-
-public class Map {
-  private ArrayList<Obj> platforms;
-  private ArrayList<Button> buttons;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+public class Map{
+  private ArrayList <Obj> objs = new ArrayList <Obj>();
   private Water water;
-  private Exit exit;
 
-  public Map(Water water, Exit exit) {
-    this.water = water;
-    this.exit = exit;
+  public Map(){
+    
   }
-  public void addPlatform(Obj platform){
-    this.platforms.add(platform);
+  public Map(ArrayList <Obj> objs){
+    this.objs = objs;
   }
-  public void addButton(Button button){
-    buttons.add(button);
-  }
-  private void load() {
-    for (int i = 0; i < platforms.size(); i++) {
-      //draw(platforms.get(i))
+  public void load(){
+    for(int i =0; i<objs.size(); i+=1){
+      // use the render method here
     }
-    for (int i = 0; i < buttons.size(); i++) {
-      //draw(buttons.get(i))
-    }
-    //draw(this.exit);
-    //draw(this.water);
+    // also render the water
   }
-
-  public void moveMap(int x, int y){
-    // Create a following camera effect for the player, making the map move around when the player moves
-  }
-
 }
-
-    // we just need to make sure each individual class and obj works
-    // then everything will be a lot easier
