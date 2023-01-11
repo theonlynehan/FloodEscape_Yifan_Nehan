703da79a0ae7502a17b1c5295e8ad3e862e92196
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2023-01-10 22:47:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.color.*;
 
-public class Main {
+class Main {
   public static void main(String[] args) {
-    // this is just for testing purposes
-    System.out.println("hi");
-    JFrame bruh = new JFrame();
-    Menu.displayMenu(bruh);
-    bruh.setVisible(true);
+    Obj[] objs = new Obj[1];
+    objs[0] = new Obj(500, 500, 50, 50, Color.green, false, false, "obj");
+    Player bruh = new Player(50, 50);
+    System.out.println(bruh.collision(objs));
   }
 }
