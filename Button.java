0e091ec581ab12532025a8b24ccbfd56e88f51c4
theonlynehan <92402146+theonlynehan@users.noreply.github.com>
2023-01-10 22:45:58 -0500

    Update Button.java

--- Button.java
+++ Button.java
@@ -1,42 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
-
-public class Button extends Obj {
-  private static ImageIcon btnNotPressed = new ImageIcon("file2.png"); // will add the images later
-  private static ImageIcon btnPressed = new ImageIcon("file.png");
-  private boolean isStopped;
-  private Water water;
-  private Player player;
-
-  public Button() {
-    super(); // this doesn't work yet
-    this.setIcon(btnNotPressed);
+public class Button extends Obj{
+  private boolean on = false;
+  public Button(int height, int width, int x, int y, Color color){
+    super(height, width, x, y, color, true, false, "button");
   }
-
-  // we will change this later so that the player checks for all collisions
-  public boolean playerWithinBounds() {
-    int playerX = player.getX();
-    int playerY = player.getY();
-
-    int buttonX = this.getX();
-    int buttonY = this.getY();
-    int buttonWidth = this.getWidth();
-    int buttonHeight = this.getHeight();
-
-    if (playerX >= buttonX && playerX <= buttonX + buttonWidth && playerY >= buttonY
-        && playerY <= buttonY + buttonHeight) {
-      return true;
-    } else {
-      return false;
-    }
+  public void turnOn(){
+    this.on = true;
   }
-
-  public void render(Graphics g) {
-    g.setColor(Color.RED);
-    g.fillRect(getX(), getY(), getWidth(), getHeight());
-    g.setColor(Color.BLACK);
-    g.drawString(getText(), getX() + getWidth() / 2, getY() + getHeight() / 2);
-  }
-
 }
